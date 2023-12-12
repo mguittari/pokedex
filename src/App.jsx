31420ca73394de3ba015e13a5ede9f0dd6c476f1
mguittari <mattias.guittari@gmail.com>
2023-12-12 17:05:55 +0100

    Bulbasaur meets Mew

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import PokemonCard from "./components/PokemonCard"
 
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    
+  },
+  {
+    name: "Mew",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png"
+  },
+];
+
 function App(){
+  
 return <div>
-      <PokemonCard/>
+      <h1>POKEDEX</h1>
+      <PokemonCard pokemon={pokemonList}/>
     </div>
 }
     
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,40 @@
+function PokemonCard({pokemon}){
 
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
+  
 
-
-function PokemonCard(){
     return <div>
-      {pokemonList.map((pokemon)=> (
+      {pokemon.map((p)=> (
         
-        pokemon.imgSrc ? (<> <img src={pokemon.imgSrc}/> 
-        <p>{pokemon.name}</p></>) : <p>{"???"}</p>
+        p.imgSrc ? (<figure> <img src={p.imgSrc}/> 
+        <figcaption>{p.name}</figcaption></figure>) : <figcaption>{"???"}</figcaption>
         
       )) }
-  
+        
     </div>
+  
 }
+
+
+/*function PokemonCard(props) {
+  
+  const { pokemon } = props;
+  const {name, imgSrc} = pokemon;
+  console.log(props);
+  
+  return (
+      <figure className="pokemon-card">
+          <figcaption>{pokemon.name}</figcaption>
+          {pokemon.imgSrc ? (
+              <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+              <p>???</p>
+          )}
+      </figure>
+  );
+}
+*/
+
+
       
     
 
