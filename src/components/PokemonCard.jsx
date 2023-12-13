4832b364850edf9a3fbd9de4b1f2b0c0dd79ca28
mguittari <mattias.guittari@gmail.com>
2023-12-13 22:47:00 +0100

    sans commentaire

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -13,35 +13,6 @@ function PokemonCard({pokemon}){
     </div>
   
 }
-
-
-/*function PokemonCard(props) {
-  
-  const { pokemon } = props;
-  const {name, imgSrc} = pokemon;
-  console.log(props);
-  
-  return (
-      <figure className="pokemon-card">
-          <figcaption>{pokemon.name}</figcaption>
-          {pokemon.imgSrc ? (
-              <img src={pokemon.imgSrc} alt={pokemon.name} />
-          ) : (
-              <p>???</p>
-          )}
-      </figure>
-  );
-}
-*/
-
-
-      
-    
-
-/*<figure>
-   <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt= "Bulbasaur"></img>
-   <figcaption>Bulbasaur</figcaption>
-</figure>*/
  
 
 export default PokemonCard;
