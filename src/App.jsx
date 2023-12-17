150b2ff77a63e00ae88eb116678ae22e27c36a1a
mguittari <mattias.guittari@gmail.com>
2023-12-17 20:37:07 +0100

    hello prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
     name: "Bulbasaur",
     imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    id: 1,
   },
   {
     name: "Mew",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png"
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+    id: 151,
   },
 ];
 
-function App(){
-  
-return <div>
+function App() {
+  return (
+    <div>
       <h1>POKEDEX</h1>
-      <PokemonCard pokemon={pokemonList}/>
+      <PokemonCard pokemon={pokemonList} />
     </div>
+  );
 }
-    
- 
+
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,30 @@
-function PokemonCard({pokemon}){
+import PropTypes from "prop-types";
 
-  
-
-    return <div>
-      {pokemon.map((p)=> (
-        
-        p.imgSrc ? (<figure> <img src={p.imgSrc}/> 
-        <figcaption>{p.name}</figcaption></figure>) : <figcaption>{"???"}</figcaption>
-        
-      )) }
-        
+function PokemonCard({ pokemon }) {
+  return (
+    <div>
+      {pokemon.map((p) =>
+        p.imgSrc ? (
+          <figure key={p.id}>
+            <img src={p.imgSrc} />
+            <figcaption>{p.name}</figcaption>
+          </figure>
+        ) : (
+          <p key={p.id}>???</p>
+        )
+      )}
     </div>
-  
+  );
 }
- 
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default PokemonCard;
