baf2847a7ec928d78fe748e79c9126d5e6852e15
mguittari <mattias.guittari@gmail.com>
2023-12-20 22:03:15 +0100

    a lot of new pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,100 @@
 import PokemonCard from "./components/PokemonCard";
-
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    id: 1,
-  },
-  {
-    name: "Mew",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
-    id: 151,
-  },
-];
+import { useState } from "react";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      id: 1,
+    },
+    {
+      name: "Charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      id: 4,
+    },
+    {
+      name: "Squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      id: 7,
+    },
+    {
+      name: "Rattata",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png",
+      id: 19,
+    },
+    {
+      name: "Pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      id: 25,
+    },
+    {
+      name: "Psyduck",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/54.png",
+      id: 54,
+    },
+    {
+      name: "Drowzee",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/96.png",
+      id: 96,
+    },
+    {
+      name: "Lapras",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/131.png",
+      id: 131,
+    },
+
+    {
+      name: "Mew",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+      id: 151,
+    },
+  ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const pokemon = pokemonList[pokemonIndex];
+
+  const handleClickNext = () => {
+    if (pokemonIndex <= pokemonList.length) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+    console.log("click");
+  };
+
+  /* setPokemonIndex((precedentIndex) =>
+    Math.min(precedentIndex + 1, pokemonList.length - 1)
+  ); */
+
+  const handleClickPrevious = () => {
+    if (pokemonIndex >= 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
   return (
     <div>
       <h1>POKEDEX</h1>
-      <PokemonCard pokemon={pokemonList} />
+      <PokemonCard pokemon={pokemon} />
+      {pokemonIndex > 0 && (
+        <button className="previous-button" onClick={handleClickPrevious}>
+          Previous
+        </button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button className="next-button" onClick={handleClickNext}>
+          Next
+        </button>
+      )}
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,26 @@
 import PropTypes from "prop-types";
 
-function PokemonCard({ pokemon }) {
+function PokemonCard(props) {
+  const { pokemon } = props;
+
   return (
-    <div>
-      {pokemon.map((p) =>
-        p.imgSrc ? (
-          <figure key={p.id}>
-            <img src={p.imgSrc} />
-            <figcaption>{p.name}</figcaption>
-          </figure>
-        ) : (
-          <p key={p.id}>???</p>
-        )
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
       )}
-    </div>
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
   );
 }
 
 PokemonCard.propTypes = {
-  pokemon: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      imgSrc: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ).isRequired,
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default PokemonCard;
